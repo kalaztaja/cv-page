0bf751b9fdec8110c36d4cdea000e5f42edcd45a
Johannes Martikkala <martikkalajohannes@gmail.com>
2021-02-11 20:50:58 +0200

    Fixed error uncaught reference in skill options

--- src/components/SkillOption.vue
+++ src/components/SkillOption.vue
@@ -31,10 +31,7 @@ export default {
     optionClicked() {
       this.$emit('selected', this.optionName);
     }
-  },
-  data: () => ({
-    logo: require('@/assets/' + this.optionName + '.png')
-  })
+  }
 };
 </script>
 
